6154d6682ba61b693b2fee9467b7f3e2e626f1ec
lordnagibator2013 <kekynkek2308@email.com>
2025-12-19 23:18:19 +1000

    jsons

--- program.cs
+++ program.cs
@@ -1,9 +1,66 @@
 using System;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SeaBattleApi.Auth;
+using SeaBattleApi.Middleware;
+using SeaBattleDB.DB;
+using SeaBattleLogic;
+using SeaBattleRepository.Implement;
 
-namespace lord13;
-
-class Main
+namespace SeaBattleApi
 {
-    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers().AddJsonOptions(s =>
+            {
+                s.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+            builder.Services.AddAuthorization();
+            builder.Services.AddControllers();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = AuthOptions.ISSUER,
+                        ValidateAudience = true,
+                        ValidAudience = AuthOptions.AUDIENCE,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<User29Context>();
+            builder.Services.AddScoped<RepositoryGame>();
+            builder.Services.AddScoped<RepositoryUser>();
+            builder.Services.AddScoped<GameLogic>();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
