780af3b5c62736557806923b4a3ff01c55a2ea8d
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-19 21:29:06 +1000

    главное окно и Vm

---
+++ SeaBattle/VM/MainVM.cs
@@ -0,0 +1,18 @@
+﻿using SeaBattle.mvvm;
+
+namespace SeaBattle.VM
+{
+    public class MainVM : BaseVM
+    {
+        private string title = "Морской бой";
+        public string Title
+        {
+            get => title;
+            set { title = value; Signal(); }
+        }
+
+        public MainVM()
+        {
+        }
+    }
+}
