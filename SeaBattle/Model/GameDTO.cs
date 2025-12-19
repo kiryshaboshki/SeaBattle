156ec27a46e469974eb8d366f11ad9c5f4ba048e
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-19 21:48:37 +1000

    добавлены DTO модели UserDTO и GameDTO

---
+++ SeaBattle/Model/GameDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SeaBattle.Models
+{
+    public class GameDTO
+    {
+        public int Id { get; set; }
+        public byte[] FieldUser1 { get; set; } = null!;
+        public byte[] FieldUser2 { get; set; } = null!;
+        public short Status { get; set; }
+        public int IdUserNextTurn { get; set; }
+        public DateTime DatetimeStartGame { get; set; }
+        public DateTime? DatetimeLastTurn { get; set; }
+        public int? IdUserWinner { get; set; }
+        public UserDTO Creator { get; set; }
+        public UserDTO Opponent { get; set; }
+    }
+}
---
+++ SeaBattle/Model/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SeaBattle.Models
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int Rating { get; set; }
+    }
+}
