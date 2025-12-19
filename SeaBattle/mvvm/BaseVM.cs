bea4e7cd19cf9c28937530826b32c8b381d9c65a
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-19 21:27:00 +1000

    базовые классы MVVM

---
+++ SeaBattle/mvvm/BaseVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SeaBattle.mvvm
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Signal([CallerMemberName] string prop = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
---
+++ SeaBattle/mvvm/CommandVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SeaBattle.mvvm
+{
+    public class CommandVM : ICommand
+    {
+        Action action;
+
+        public CommandVM(Action action)
+        {
+            this.action = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter) => action();
+    }
+}
