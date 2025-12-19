cde6065c0f26f1c77f1fb8bea2b96e1ac0f1a3b4
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-19 21:30:53 +1000

    PageControl для управления навигацией между страницами

---
+++ SeaBattle/VM/PageControl.cs
@@ -0,0 +1,30 @@
+﻿using SeaBattle.mvvm;
+using System.Windows.Controls;
+
+namespace SeaBattleWPF.VM
+{
+    public class PageControl : BaseVM
+    {
+        private static PageControl instance;
+
+        private Page currentPage;
+        public Page CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                Signal();
+            }
+        }
+
+        private PageControl() { }
+
+        public static PageControl GetInstance()
+        {
+            if (instance == null)
+                instance = new PageControl();
+            return instance;
+        }
+    }
+}
