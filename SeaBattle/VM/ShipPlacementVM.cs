2ff007e27a3a96a3819a1e21759dfdff5ebfb472
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-19 22:55:37 +1000

    добавлена страница расстановки кораблей согласно ТЗ

---
+++ SeaBattle/VM/ShipPlacementVM.cs
@@ -0,0 +1,66 @@
+﻿using SeaBattle.Models;
+using SeaBattle.mvvm;
+using SeaBattle.View;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SeaBattle.VM
+{
+    public class ShipPlacementVM : BaseVM
+    {
+        public ObservableCollection<ShipModel> AvailableShips { get; set; }
+
+        private bool isHorizontal = true;
+        public bool IsHorizontal
+        {
+            get => isHorizontal;
+            set { isHorizontal = value; Signal(); }
+        }
+
+        public CommandVM PlaceShipCommand { get; set; }
+        public CommandVM ClearFieldCommand { get; set; }
+        public CommandVM StartGameCommand { get; set; }
+
+        public ShipPlacementVM()
+        {
+            AvailableShips = new ObservableCollection<ShipModel>
+            {
+                new ShipModel { Size = 4, Count = 1, Description = "4-палубный (1 шт)" },
+                new ShipModel { Size = 3, Count = 2, Description = "3-палубный (2 шт)" },
+                new ShipModel { Size = 2, Count = 3, Description = "2-палубный (3 шт)" },
+                new ShipModel { Size = 1, Count = 4, Description = "1-палубный (4 шт)" }
+            };
+
+            PlaceShipCommand = new CommandVM(() =>
+            {
+                MessageBox.Show("Корабль размещен (заглушка)");
+                // Здесь будет логика размещения
+            });
+
+            ClearFieldCommand = new CommandVM(() =>
+            {
+                MessageBox.Show("Поле очищено");
+            });
+
+            StartGameCommand = new CommandVM(() =>
+            {
+                MessageBox.Show("Начинаем игру!");
+                var pageControl = PageControl.GetInstance();
+                pageControl.CurrentPage = new GamePage();
+            });
+        }
+
+        public void SelectCell(int x, int y)
+        {
+            // Логика выбора клетки для размещения
+        }
+    }
+
+    public class ShipModel
+    {
+        public int Size { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ SeaBattle/View/ShipPlacementPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SeaBattle.View
+{
+    public partial class ShipPlacementPage : Page
+    {
+        public ShipPlacementPage()
+        {
+            InitializeComponent();
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            PlacementField.Children.Clear();
+
+            // Рисуем сетку
+            for (int i = 0; i <= 10; i++)
+            {
+                var verticalLine = new Line
+                {
+                    X1 = i * 30,
+                    Y1 = 0,
+                    X2 = i * 30,
+                    Y2 = 300,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1
+                };
+
+                var horizontalLine = new Line
+                {
+                    X1 = 0,
+                    Y1 = i * 30,
+                    X2 = 300,
+                    Y2 = i * 30,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1
+                };
+
+                PlacementField.Children.Add(verticalLine);
+                PlacementField.Children.Add(horizontalLine);
+            }
+        }
+
+        private void Field_MouseClick(object sender, MouseButtonEventArgs e)
+        {
+            var pos = e.GetPosition(PlacementField);
+            int x = (int)(pos.X / 30);
+            int y = (int)(pos.Y / 30);
+
+            if (x >= 0 && x < 10 && y >= 0 && y < 10)
+            {
+                var vm = (VM.ShipPlacementVM)DataContext;
+                vm.SelectCell(x, y);
+            }
+        }
+    }
+}
