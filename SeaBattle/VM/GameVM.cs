d525ebcf0ab0a623174c10ca0d45aa99b2da8d9c
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-19 22:31:24 +1000

    создана базовая игровая страница с макетом полей

---
+++ SeaBattle/VM/GameVM.cs
@@ -0,0 +1,34 @@
+﻿using SeaBattle.mvvm;
+using System;
+
+namespace SeaBattle.VM
+{
+    public class GameVM : BaseVM
+    {
+        private string gameStatus = "Ожидание хода";
+        public string GameStatus
+        {
+            get => gameStatus;
+            set { gameStatus = value; Signal(); }
+        }
+
+        private string currentPlayer = "Вы";
+        public string CurrentPlayer
+        {
+            get => currentPlayer;
+            set { currentPlayer = value; Signal(); }
+        }
+
+        public CommandVM ExitCommand { get; set; }
+
+        public GameVM()
+        {
+            ExitCommand = new CommandVM(() =>
+            {
+                // Здесь будет выход из игры
+                var pageControl = PageControl.GetInstance();
+                pageControl.CurrentPage = new View.PageListGames();
+            });
+        }
+    }
+}
---
+++ SeaBattle/View/GamePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SeaBattle.View
+{
+    public partial class GamePage : Page
+    {
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
