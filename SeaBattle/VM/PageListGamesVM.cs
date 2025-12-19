f48e19b071fa543d17a17c645fe26a1ca3d61e7b
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-19 22:23:09 +1000

    добавлена страница списка игр с базовой структурой

---
+++ SeaBattle/VM/PageListGamesVM.cs
@@ -0,0 +1,39 @@
+﻿using SeaBattle.API;
+using SeaBattle.mvvm;
+using SeaBattle.View;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SeaBattle.VM
+{
+    public class PageListGamesVM : BaseVM
+    {
+        public CommandVM CreateGameCommand { get; set; }
+        public CommandVM RefreshCommand { get; set; }
+
+        public PageListGamesVM()
+        {
+            CreateGameCommand = new CommandVM(async () =>
+            {
+                var result = await Client.Instance.PostAsync("GameInfo/CreateGame");
+
+                if (result.Success)
+                {
+                    MessageBox.Show("Игра создана! Ожидаем соперника...");
+                    // Здесь позже будет переход на игровую страницу
+                }
+                else
+                {
+                    MessageBox.Show($"Ошибка создания игры: {result.Response}");
+                }
+            });
+
+            RefreshCommand = new CommandVM(async () =>
+            {
+                MessageBox.Show("Обновляем список игр...");
+                // Здесь позже будет загрузка списка игр
+            });
+        }
+    }
+}
---
+++ SeaBattle/View/PageListGames.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SeaBattle.View
+{
+    public partial class PageListGames : Page
+    {
+        public PageListGames()
+        {
+            InitializeComponent();
+        }
+    }
+}
